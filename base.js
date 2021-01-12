5218d750c37d7792866bb28e8c32fcd100a897c1
Michael <micjanic20@gmail.com>
2021-01-11 23:20:50 -0600

    set up draw method

--- base.js
+++ base.js
@@ -3,8 +3,30 @@ const ctx = canvas.getContext('2d')
 
 let state = initalState()
 
+const x = (c) => Math.round((c * canvas.width) / state.cols)
+const y = (r) => Math.round((r * canvas.height) / state.rows)
+
+const draw = () => {
+    //clear
+    ctx.fillStyle = '#232323'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    //draw snake
+    ctx.fillStyle = 'rgb(0,200,50)'
+    state.snake.map((p) => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)))
+
+    //draw apples
+    ctx.fillStyle = 'rgb(255, 50, 0)'
+    ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1))
+
+    //add crash
+    if (state.snake.length === 0) {
+        ctx.fillStyle = 'rgb(255,50,0)'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+    }
+}
+
 const step = (t1) => {
-    debugger
     return (t2) => {
         if (t2 - t1 > 100) {
             state = next(state)
@@ -16,4 +38,5 @@ const step = (t1) => {
     }
 }
 
+draw()
 window.requestAnimationFrame(step(0))
